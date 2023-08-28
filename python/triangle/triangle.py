bacb7ae6d145adc9764ebcef832256469a00d821
unknown <abarghouei.mahsa@gmail.com>
2023-08-28 14:45:16 +0330

    completed triangle

---
+++ python/triangle/triangle.py
@@ -0,0 +1,17 @@
+def equilateral(sides):
+    a, b, c = sides
+    return is_triangle(sides) and a == b == c
+        
+
+def isosceles(sides):
+    a, b, c = sides
+    return is_triangle(sides) and (a == b or a == c or b == c)
+        
+
+def scalene(sides):
+    a, b, c = sides
+    return is_triangle(sides) and a != b and a!= c and b != c
+
+def is_triangle(sides):
+    a, b, c = sides
+    return a + b >= c and b + c >= a and a + c >= b and a > 0 and b > 0 and c > 0
